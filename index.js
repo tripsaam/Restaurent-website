30d6da461d835aaaa6e2f2435f67f3bb992613d0
tripsaam <cs1744.iiitbhopal@gmail.com>
2021-08-16 15:28:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+$(".night-mode").click(function(){
+  $(".colored-section").addClass("night-mode-color");
+  $(".white-section").addClass("night-mode-white");
+  // $(".night-mode").html("<i class="fas fa-sun"></i>");
+});
